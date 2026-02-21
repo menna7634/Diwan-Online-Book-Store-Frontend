b3bc78bc1d70e6b41415356a197b5a27a141b52c
Mostafa-Khalifaa <mostafa.khalifa.2000.mk@gmail.com>
2026-02-21 16:35:57 +0200

    feat(ui): add new home page

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [RouterModule],
+  templateUrl: './home.html',
+})
+export class Home {}
