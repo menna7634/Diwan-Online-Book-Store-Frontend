109d5bac45f92b9e6b5b996d9a8020665c197106
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-22 23:08:23 +0200

    implement a simple interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideAppInitia
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthService } from './core/services/auth.service';
+import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideAppInitializer(() => {
       const authService = inject(AuthService);
       return authService.hydrate();
