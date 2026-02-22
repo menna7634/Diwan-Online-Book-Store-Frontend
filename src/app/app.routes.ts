01ba042f430880c2309919d2e0e7d5ffa10c407b
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-22 17:03:00 +0200

    add login and register to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { LoginPage } from './features/auth/login/login.page';
+import { RegisterPage } from './features/auth/register/register.page';
 
-export const routes: Routes = [{ path: '', component: Home }];
+export const routes: Routes = [
+  { path: '', component: Home },
+  {
+    path: "login",
+    component: LoginPage
+  },
+  {
+    path: "register",
+    component: RegisterPage,
+  }
+];
