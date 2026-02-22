be9e00393a93b6fb214b4ee4e68ef73bb91b2693
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-22 23:08:53 +0200

    implement guest guards for login and register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
 import { LoginPage } from './features/auth/login/login.page';
 import { RegisterPage } from './features/auth/register/register.page';
+import { guestGuard } from './core/guards/guest-guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   {
     path: "login",
-    component: LoginPage
+    component: LoginPage,
+    canActivate: [guestGuard]
   },
   {
     path: "register",
     component: RegisterPage,
+    canActivate: [guestGuard]
   }
 ];
