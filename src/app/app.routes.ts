bcf08e5380ed8a9e0875df52a8bd7d46c14fbadf
Zeyad Shahin <zeyadshahin2015@gmail.com>
2026-02-25 15:04:28 +0200

    add routes for the admin pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,12 @@ import { Home } from './features/home/home';
 import { LoginPage } from './features/auth/login/login.page';
 import { RegisterPage } from './features/auth/register/register.page';
 import { guestGuard } from './core/guards/guest-guard';
+import { AdminPanelLayout } from './core/features/admin/admin-panel/admin-panel.layout';
+import { DashboardPage } from './core/features/admin/dashboard/dashboard.page';
+import { OrdersPage } from './core/features/admin/orders/orders.page';
+import { AuthorsPage } from './core/features/admin/authors/authors.page';
+import { CategoriesPage } from './core/features/admin/categories/categories.page';
+import { BooksPage } from './core/features/admin/books/books.page';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -15,5 +21,36 @@ export const routes: Routes = [
     path: "register",
     component: RegisterPage,
     canActivate: [guestGuard]
+  },
+  {
+    path: "admin",
+    component: AdminPanelLayout,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardPage,
+      },
+      {
+        path: 'orders',
+        component: OrdersPage,
+      },
+      {
+        path: 'authors',
+        component: AuthorsPage,
+      },
+      {
+        path: 'categories',
+        component: CategoriesPage,
+      },
+      {
+        path: 'books',
+        component: BooksPage,
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
